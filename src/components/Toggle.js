97164b4dd7cc29c8a8b00048e93993e4dab98c5f
Serje <Serje.havandjian@gmail.com>
2022-05-18 09:21:57 -0400

    commit tree 2b1b52ed1fd78913a65ad3f3d7d58ffbc86c19f6

--- src/components/Toggle.js
+++ src/components/Toggle.js
@@ -1,7 +1,40 @@
-import React from "react";
+import React, {useState} from "react";
 
 function Toggle() {
-  return <button>OFF</button>;
+
+
+  //SERJE'S SOLUTION
+  let butonStatus = "OFF"
+    
+  const [count, setCount] = useState(0)
+
+    function increment(){
+      setCount((currentCount)=> currentCount+1)
+    }
+    
+    if(count%2 ===0){
+      butonStatus ="ON" 
+    } else {
+      butonStatus ="OFF"
+    }
+  return <button onClick={increment}>{butonStatus}</button>;
+
+
+
+  // const [isOn, setIsOn] = useState(false)
+
+  // function handleClick(){
+  //   setIsOn((isOn) => !isOn)
+  // }
+
+  // const color = isOn ? "red" : "white"
+
+  // return(
+  // <button
+  //   style={{background: color}}
+  //   onClick={handleClick}>{isOn ? "ON" : "OFF"}
+  // </button>
+  // )
 }
 
 export default Toggle;
